e57e118e6705daf08dfa8c96b25aa5d91775d19b
Nada44yehia <109657564+Nada44yehia@users.noreply.github.com>
2026-04-02 20:12:42 +0200

    commit

--- environment.sv
+++ environment.sv
@@ -5,38 +5,38 @@ class environment;
 	driver 	   drv ;
 	monitor    mont;
 	scoreboard scb ;
-        subscriber sub;
-        event drv_done;
+    subscriber sub;
+    event drv_done;
 
 
 	virtual memory_intf vif_mem;
 	
 	mailbox mon_to_scb;
-        mailbox mon_to_sub;
+    mailbox mon_to_sub;
   	mailbox seq_to_dr;
   	
   function new(virtual memory_intf vif_mem);
-                this.vif_mem = vif_mem;
+        this.vif_mem = vif_mem;
 		mon_to_scb = new();
-                mon_to_sub = new();
-                seq_to_dr= new();
+        mon_to_sub = new();
+        seq_to_dr= new();
 		Seq  = new(seq_to_dr,drv_done);
 		drv  = new(vif_mem,seq_to_dr,drv_done);
 		mont = new(vif_mem,mon_to_scb,mon_to_sub);
 		scb  = new(mon_to_scb); 
-                sub = new(vif_mem, mon_to_sub); 	
+        sub = new(mon_to_sub); 	
 		
    endfunction
 
  virtual task run();
         
-       fork
+    fork
 		
 	Seq.run_seq();
 	drv.run_drive();
 	mont.run_monitor();
 	scb.run_scoreboard();
-        sub.run_subscriber();
+    sub.run_subscriber();
           	
 		
 	join_any
